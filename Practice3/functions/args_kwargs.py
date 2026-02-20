9a3d7469d755328e10c6206b5593c366698be363
Nurkozha0607 <n_kadyrbek@kbtu.kz>
2026-02-20 07:10:07 +0500

    first

---
+++ Practice3/functions/args_kwargs.py
@@ -0,0 +1,61 @@
+# What is args
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
+
+# individual arguments from *args.    
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
+
+# Regular arguments
+def my_function(*numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  return total
+
+print(my_function(1, 2, 3))
+print(my_function(10, 20, 30, 40))
+print(my_function(5))
+
+# What is kwargs
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
+
+# Combining *args and **kwargs
+def my_function(title, *args, **kwargs):
+  print("Title:", title)
+  print("Positional arguments:", args)
+  print("Keyword arguments:", kwargs)
+
+my_function("User Info", "Emil", "Tobias", age = 25, city = "Oslo")
+
+# Unpacking Arguments
+def my_function(a, b, c):
+  return a + b + c
+
+numbers = [1, 2, 3]
+result = my_function(*numbers) # Same as: my_function(1, 2, 3)
+print(result)
+
+# Unpacking Dictionaries with **
+
+def my_function(fname, lname):
+  print("Hello", fname, lname)
+
+person = {"fname": "Emil", "lname": "Refsnes"}
+my_function(**person) # Same as: my_function(fname="Emil", lname="Refsnes")
---
+++ Practice3/lambda/lambda_basics.py
@@ -0,0 +1,7 @@
+# syntax
+x = lambda a : a + 10
+print(x(5))
+
+# Multiply argument 
+x = lambda a, b : a * b
+print(x(5, 6))
